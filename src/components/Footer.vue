3e3a4ccf387aba6f41f33b0a9167f50ef26afb50
Yupi7 <yupi@EEkiciNB01.intern.freicon.de>
2024-09-02 13:14:15 +0200

    Links zu SM

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,12 +1,26 @@
 <template>
   <footer>
-    <p>&copy; 2024 Kleiderspende Verein - Alle Rechte vorbehalten.</p>
+    <p>&copy; 2024 IU Kleiderspende e.V. - Alle Rechte vorbehalten.</p>
+    <div class="social-links">
+      <a :href="facebookUrl" target="_blank" rel="noopener noreferrer">
+        <img src="@/assets/facebook-icon.png" alt="Facebook" />
+      </a>
+      <a :href="instagramUrl" target="_blank" rel="noopener noreferrer">
+        <img src="@/assets/instagram-icon.png" alt="Instagram" />
+      </a>
+    </div>
   </footer>
 </template>
 
 <script>
 export default {
-  name: 'Footer'
+  name: 'Footer',
+  data() {
+    return {
+      facebookUrl: 'https://www.facebook.com',
+      instagramUrl: 'https://www.instagram.com/yourprofile',
+    };
+  }
 }
 </script>
 
@@ -16,5 +30,19 @@ footer {
   color: black;
   text-align: center;
   padding: 10px;
+}
+
+.social-links {
+  margin-top: 10px;
+}
+
+.social-links a {
+  margin: 0 10px;
+  display: inline-block;
+}
+
+.social-links img {
+  width: 24px;
+  height: 24px;
 }
 </style>
